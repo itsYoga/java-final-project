18d41fb8e8427e193cb77e23712eb26c195dbce8
01157121 <01157121@email.ntou.edu.tw>
2024-05-08 15:43:13 +0800

    更新資料

--- FirstClass22Team.java
+++ FirstClass22Team.java
@@ -2,5 +2,6 @@ public class FirstClass22Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157145\"\n姓名：\"梁祐嘉\"");
+        System.out.println("\"學號：\"01157121\"\n姓名：\"吳至睿\"");
     }
 }
