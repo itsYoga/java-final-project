2eec1dce08e7f65e44541adcb6751d7ac76d2a68
ttcpsntpc <ianfvx82678@gmail.com>
2024-05-08 15:45:59 +0800

    Update FirstClass22Team.java

--- FirstClass22Team.java
+++ FirstClass22Team.java
@@ -3,5 +3,6 @@ public class FirstClass22Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157145\"\n姓名：\"梁祐嘉\"");
         System.out.println("\"學號：\"01157121\"\n姓名：\"吳至睿\"");
+        System.out.println("\"學號：\"01157127\"\n姓名：\"洪宗邑\"");
     }
 }
